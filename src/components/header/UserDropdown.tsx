98223f68fd9152db450411002212142b52943ef0
Nguyễn Thành <thanhngune123@gmail.com>
2025-07-26 21:47:08 +0700

    re-upload ver1

--- src/components/header/UserDropdown.tsx
+++ src/components/header/UserDropdown.tsx
@@ -23,7 +23,7 @@ export default function UserDropdown() {
           <img src="/images/user/owner.jpg" alt="User" />
         </span>
 
-        <span className="block mr-1 font-medium text-theme-sm">Musharof</span>
+        <span className="block mr-1 font-medium text-theme-sm">Nexus Zone</span>
         <svg
           className={`stroke-gray-500 dark:stroke-gray-400 transition-transform duration-200 ${
             isOpen ? "rotate-180" : ""
@@ -51,10 +51,10 @@ export default function UserDropdown() {
       >
         <div>
           <span className="block font-medium text-gray-700 text-theme-sm dark:text-gray-400">
-            Musharof Chowdhury
+            Nexus Zone
           </span>
           <span className="mt-0.5 block text-theme-xs text-gray-500 dark:text-gray-400">
-            randomuser@pimjo.com
+            NexusZone@gmail.com
           </span>
         </div>
 
